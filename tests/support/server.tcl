2150d7f2bee05ccb889b29b9337675b52abe2c65
michalbiesek <michal.biesek@intel.com>
2020-11-05 09:19:37 +0100

    Add --pmem-ratio option for runtest
    - to test ratio configuration run: ./runtest --pmem-ratio
    - allows to run test for configuration including ratio policy
    - update default.conf with following values:
        memory-alloc-policy ratio
        dram-pmem-ratio 1 3
        initial-dynamic-threshold 64
        memory-ratio-check-period 100
        hashtable-on-dram yes

--- tests/support/server.tcl
+++ tests/support/server.tcl
@@ -158,6 +158,15 @@ proc start_server {options {code undefined}} {
 
     # setup defaults
     set baseconfig "default.conf"
+    if {$::pmem_ratio_test} {
+        set memory-alloc-policy "ratio"
+        set dram-pmem-ratio "1 3"
+        set initial-dynamic-threshold "64"
+        set dynamic-threshold-min "24"
+        set dynamic-threshold-max "10000"
+        set memory-ratio-check-period "100"
+        set hashtable-on-dram "yes"
+    }
     set overrides {}
     set tags {}
 
